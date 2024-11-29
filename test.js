9b75ad665abf02f46dae27c364e36ecfc7e5a4dc
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-11-28 23:34:27 -0300

    test

--- test.js
+++ test.js
@@ -1,18 +1,33 @@
-import fs from 'fs'
+import puppeteer from "puppeteer";
+import colors from 'ansi-colors'
+import readline from 'readline'
 
-function addingData() {
-    const myJson = fs.readFileSync('./unknow.json')
-    const parseData = JSON.parse(myJson)
-    parseData[1][1].pages =  "16" 
-
+export async function test(reqPage, tag) {
+     
+     console.log(colors.bgGreen('Loading...'))
     
-    const newJson = JSON.stringify(parseData)
-
-    fs.writeFile('./unknow.json', newJson, err =>{
-        if(err) throw new err
-
-        console.log('Data Added')
-    })
+    const browser = await puppeteer.launch({
+        headless: true,
+      });
+      // New instance of BROWSER (not the current in use)
+      const page = await browser.newPage();
+  
+      await page.goto(reqPage);
+      const elementHandle = await page.$$(tag)
+      const text  = await Promise.all(elementHandle.map(el=> el.evaluate((node) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+          });
+          rl.question(colors.cyan('If you want get an attribute: '), (input) => {
+            node.getAttribute(input); // Pass the input to a function
+            rl.close(); // Close the interface
+          });
+        } 
+    
+    )))
+      console.log(text, (colors.bgGreen('Done! 😊')))
+      await browser.close();
 }
 
-addingData()
+// test(`https://www.twpornstars.com`, 'img')
