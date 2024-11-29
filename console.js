50105d24a0613f4f64e2cbd7e5ad5d18651e6074
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-11-28 23:34:56 -0300

    near to get the final code

--- console.js
+++ console.js
@@ -1,17 +1,51 @@
 import readline from 'readline'
 import colors from 'ansi-colors'
+import { test } from './test.js';
 
-function prompeteer() {
-   
+///////////////////////////////////////////////////////////////////////////////////
+  
+  async function userValue() {
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
-    })
+        output: process.stdout,
+      });
+ console.log('🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐 Scrap To JSON 🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐')
+    // Ask the user for input
+  rl.question(colors.cyan('Please insert the url: '), (answer) => {
+    test(answer); // Pass the input to a function
+    rl.close(); // Close the interface
+  });
+  }
+  
+// userValue()
 
-    rl.question('Enter something ', (answer)=>{
-        console.log( colors.red(`You entered: ${answer}`))
-        rl.close()
-    })
-}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  let values = []; // Array to store the values
+  
+  function askForValue(param = 'url') {
 
-prompeteer()
+   // console.log('🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐', colors.red('Scrap To JSON') ,'🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐🌐')
+
+    rl.question(colors.cyan(`Enter a ${param}: `), (input) => {
+     
+      if (values.length >= 1) {
+        values.push(input);
+        test(values[0], values[1])
+        rl.close();
+      } else {
+        values.push(input); // Add input to the array
+        
+            askForValue('DOM Element tag (without "<>") or selector'); // Ask again
+        
+        
+      }
+    });
+  }
+  
+  
+  
+  askForValue();
